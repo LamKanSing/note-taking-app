62c4cbae2808511226413ccf2cda1c15a4ae50b8
lamkansing <lam.kan.sing@gmail.com>
2015-05-28 20:13:18 +0800

    wtf-form

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+app.config['SECRET_KEY']='devkey'
 Bootstrap(app)
+
+
+class MyForm(Form):
+    idea = StringField('idea', validators=[DataRequired()])
+    submit_button = SubmitField('add idea')
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    form = MyForm()
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)

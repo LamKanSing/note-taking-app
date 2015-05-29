f6eac8bc0029c492940c7bd5ecc79e8f3bb3d60f
lamkansing <lam.kan.sing@gmail.com>
2015-05-29 19:07:07 +0800

    database

--- app.py
+++ app.py
@@ -5,11 +5,27 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask.ext.sqlalchemy import SQLAlchemy
+
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='devkey'
+app.config['SECRET_KEY'] = 'devkey'
+app.config['DATABASE_URL'] = os.environ['DATABASE_URL']
 Bootstrap(app)
 
+db = SQLAlchemy(app)
+db.create_all()
+
+class Idea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idea = db.Column(db.String(256), unique=True)
+
+    def __init__(self, idea):
+        self.idea = idea
+
+    def __repr__(self):
+        return '<Idea %r>' % self.id
 
 class IdeaForm(Form):
     idea = StringField('idea', validators=[DataRequired()])
